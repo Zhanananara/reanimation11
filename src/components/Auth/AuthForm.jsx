b53defe60de4be1a19d6e80130ecfbbffd53449a
Zhanananara <zhannara.nurlanova@gmail.com>
2022-06-28 13:23:28 +0600

    api changed1

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -23,10 +23,6 @@ function Copyright(props) {
       align="center"
       {...props}
     >
-      {"Copyright © "}
-      <Link color="inherit" href="https://chanel.com/">
-        Chanel.com
-      </Link>{" "}
       {new Date().getFullYear()}
       {"."}
     </Typography>
@@ -58,9 +54,6 @@ export default function AuthForm({
           sm={4}
           md={7}
           sx={{
-            backgroundImage: `url(https://images.unsplash.com/photo-1591892204579-530edbd4724d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80),
-              url(https://images.unsplash.com/photo-1594913785077-d2d533571a78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)`,
-            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
                 ? t.palette.grey[50]
